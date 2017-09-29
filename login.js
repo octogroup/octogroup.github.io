1de0c7e2ac0ace2d0e364b405058f320d167558a
Hugo Rune <31449959+rawnney@users.noreply.github.com>
2017-09-29 14:02:21 +0200

    Add working login.js, minor html cleanup + errortxt

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
-let modal = document.getElementById('loginbox');
 
+let modal = document.getElementById('loginbox');
 // Stäng modul om användaren klickar utanför loginbox
 window.onclick = function(event) {
     if (event.target == modal) {
@@ -7,17 +7,33 @@ window.onclick = function(event) {
     }
 }
 
-//Använd Switch?
-//Använd let accesslevel => ?
-
-accesslevel = function () {
-  if (unameinput === "student" && pswinput === "pw") {
-    form.action == "studentpage.html";
-  } else if (unameinput === "teacher" && pswinput === "pw") {
-    form.action == "teacherpage.html";
-  } else if (unameinput === "admin" && pswinput === "pw") {
-    form.action == "adminpage.html";
+function accesslevel(form) {
+  if (form.unameinput.value === "student" && form.pswinput.value === "pw") {
+    window.open('./testpages/studentpage.html');
+  } else if (form.unameinput.value === "teacher" && form.pswinput.value === "pw") {
+    window.open('./testpages/teacherpage.html');
+  } else if (form.unameinput.value == "admin" && form.pswinput.value == "pw") {
+    window.open('./testpages/adminpage.html');
   } else {
+    document.getElementById('errortxt').style.display='flex';
     return;
   }
 }
+
+//--------------------------SWITCH-----------------------
+// switch (form) {
+//   case:
+//     form.unameinput.value == "student" && form.pswinput.value == "pw";
+//     window.open('./testpages/studentpage.html');
+//     break;
+//   case:
+//     form.unameinput.value == "teacher" && form.pswinput.value == "pw";
+//     window.open('./testpages/teacherpage.html')
+//     break;
+//   case:
+//     form.unameinput.value == "admin" && form.pswinput.value == "pw"
+//     window.open('./testpages/adminpage.html')
+//     break;
+//   default:
+//     document.getElementById('errortxt').style.display='flex'
+// }
