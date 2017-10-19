a3aee6eb3d1ffdfc938553bfabe7ea3f8ead441d
elin kviberg <elin_kviberg@hotmail.com>
2017-10-19 15:04:15 +0200

    insert the JS-code into dailyfeedback.js

--- javascript/dailyfeedback.js
+++ javascript/dailyfeedback.js
@@ -22,3 +22,20 @@ function showAnswerOnSend() {
 function hideAnswerOnSend() {
 	document.getElementById('answerOnSend').style.display='none';
 }
+function sad() {
+	document.getElementById("ledsenSmiley").src = "../img/sad.png";
+}
+
+function neutral() {
+	document.getElementById("neutralSmiley").src = "../img/confused.png";
+}
+
+function happy() {
+	document.getElementById("happySmiley").src = "../img/happyy.png";
+}
+
+function resetSmileys() {
+	document.getElementById("ledsenSmiley").src = "../img/sadDIs.png";
+	document.getElementById("neutralSmiley").src = "../img/confusedDis.png";
+	document.getElementById("happySmiley").src = "../img/happyyDis.png";
+}
