ed245e653c4a138fbfc678eda7a3187f74f6a2db
Svahnen <svahn.jonny@gmail.com>
2017-10-20 12:42:05 +0200

    create dialog window

---
+++ javascript/popup.js
@@ -0,0 +1,9 @@
+let openPopup = function () {
+  document.getElementById("shadowbg").style.display = "block";
+  document.getElementById("popupwindow").style.display = "block";
+}
+
+let closePopup = function () {
+  document.getElementById("shadowbg").style.display = "none";
+  document.getElementById("popupwindow").style.display = "none";
+}
