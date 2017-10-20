84054d06bf7ae32a6c3a0d0c908f4d8822320887
Hugo Rune <31449959+rawnney@users.noreply.github.com>
2017-10-20 15:43:13 +0200

    start implementing dialog on cards

--- javascript/forms.js
+++ javascript/forms.js
@@ -16,6 +16,8 @@ let i4 = document.getElementById("i4");
 let i5 = document.getElementById("i5");
 let sB = document.getElementById("submit");
 let cB = document.getElementById("close");
+let weekeval = document.getElementById('weekeval');
+let courseFeedback = document.getElementById('courseFeedback');
 
 // Reset function for the close button and for use on the first line of each form function
 let reset = function() {
@@ -45,32 +47,8 @@ document.getElementById('close').onclick = function() {
 };
 
 // After this line comes all the form functions
-document.getElementById('news').onclick = function() {
-  reset();
-  cB.style.display = "inherit";
-  p1.innerHTML = "Dagens nyheter är:";
-  t1.style.display = "inherit";
-  t1.style.height = "200px";
-  t1.style.width = "350px";
-  t1.required = true;
-  sB.style.display = "inherit";
-};
 
-document.getElementById('feedback').onclick = function() {
-  reset();
-  cB.style.display = "inherit";
-  p1.innerHTML = "Vad har varit bra/dåligt under dagen?";
-  t1.style.display = "inherit";
-  t1.style.width = "350px";
-  t1.required = true;
-  p2.innerHTML = "Hur har klimatet i klassen varit?";
-  t2.style.display = "inherit";
-  t2.style.width = "350px";
-  t2.required = true;
-  sB.style.display = "inherit";
-};
-
-document.getElementById('weekeval').onclick = function() {
+function news() {
   reset();
   cB.style.display = "inherit";
   p1.innerHTML = "Ditt namn (frivilligt):";
@@ -79,14 +57,53 @@ document.getElementById('weekeval').onclick = function() {
   p2.innerHTML = "Hur upplever du arbetsmiljön?";
   i2.style.display = "inherit";
   i2.type = "range";
-  i2.style.width = "350px";
+  i2.style.width = "340px";
   p3.innerHTML = "Hur upplever du just nu tempot i utbildningen?";
   i3.style.display = "inherit";
   i3.type = "range";
-  i3.style.width = "350px";
+  i3.style.width = "340px";
   p4.innerHTML = "Förslag på förbättringar:";
   t4.style.display = "inherit";
   t4.style.height = "150px";
-  t4.style.width = "350px";
+  t4.style.width = "340px";
+  sB.style.display = "inherit";
+};
+
+courseFeedback.onclick = function() {
+  reset();
+  cB.style.display = "inherit";
+  p1.innerHTML = "Hur tycker du att kursen har varit?";
+	i1.style.display = "inherit";
+  i1.type = "range";
+  i1.style.width = "340px";
+  p2.innerHTML = "Hur har lokalerna varit?";
+	i2.style.display = "inherit";
+  i2.type = "range";
+  i2.style.width = "340px";
+	p3.innerHTML = "	Finns det något mer som du vill framföra?";
+	t3.style.display = "inherit";
+  t3.style.height = "150px";
+  t3.style.width = "340px";
+  sB.style.display = "inherit";
+};
+
+weekeval.onclick = function() {
+  reset();
+  cB.style.display = "inherit";
+  p1.innerHTML = "Ditt namn (frivilligt):";
+  t1.style.display = "inherit";
+  t1.style.width = "200px";
+  p2.innerHTML = "Hur upplever du arbetsmiljön?";
+  i2.style.display = "inherit";
+  i2.type = "range";
+  i2.style.width = "340px";
+  p3.innerHTML = "Hur upplever du just nu tempot i utbildningen?";
+  i3.style.display = "inherit";
+  i3.type = "range";
+  i3.style.width = "340px";
+  p4.innerHTML = "Förslag på förbättringar:";
+  t4.style.display = "inherit";
+  t4.style.height = "150px";
+  t4.style.width = "340px";
   sB.style.display = "inherit";
 };
