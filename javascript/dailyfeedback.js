4ef0c7cf6201beac0cda393cb6285f855d4516dc
elin kviberg <elin_kviberg@hotmail.com>
2017-10-20 14:33:20 +0200

    change the current checked radiobutton/smiley-image to yellow

--- javascript/dailyfeedback.js
+++ javascript/dailyfeedback.js
@@ -22,20 +22,50 @@ function showAnswerOnSend() {
 function hideAnswerOnSend() {
 	document.getElementById('answerOnSend').style.display='none';
 }
-function sad() {
+
+
+//changes the current smiley image to yellow and all the other smiley images to grey onmouseover and onclick
+function checkSadSmiley () {
 	document.getElementById("ledsenSmiley").src = "../img/sad.png";
+	document.getElementById("neutralSmiley").src = "../img/confusedDis.png";
+	document.getElementById("happySmiley").src = "../img/happyyDis.png";z
 }
 
-function neutral() {
+function checkNeutralSmiley () {
 	document.getElementById("neutralSmiley").src = "../img/confused.png";
+	document.getElementById("ledsenSmiley").src = "../img/sadDis.png";
+	document.getElementById("happySmiley").src = "../img/happyyDis.png";
+	
 }
 
-function happy() {
+function checkHappySmiley () {
 	document.getElementById("happySmiley").src = "../img/happyy.png";
+	document.getElementById("ledsenSmiley").src = "../img/sadDis.png";
+	document.getElementById("neutralSmiley").src = "../img/confusedDis.png";
+
 }
+
 
 function resetSmileys() {
-	document.getElementById("ledsenSmiley").src = "../img/sadDIs.png";
-	document.getElementById("neutralSmiley").src = "../img/confusedDis.png";
-	document.getElementById("happySmiley").src = "../img/happyyDis.png";
+	var sadOptionIsChecked = document.getElementById("ledsen").checked;
+	var neutralOptionIsChecked = document.getElementById("neutral").checked;
+	var happyOptionIsChecked = document.getElementById("glad").checked;
+
+	if(sadOptionIsChecked == true){
+		document.getElementById("ledsenSmiley").src = "../img/sad.png";
+	}else{
+		document.getElementById("ledsenSmiley").src = "../img/sadDis.png";
+	}
+
+	if(neutralOptionIsChecked == true){
+		document.getElementById("neutralSmiley").src = "../img/confused.png";
+	}else{
+		document.getElementById("neutralSmiley").src = "../img/confusedDis.png";
+	}
+
+	if(happyOptionIsChecked == true){
+		document.getElementById("happySmiley").src = "../img/happyy.png";
+	}else{
+		document.getElementById("happySmiley").src = "../img/happyyDis.png";
+	}
 }
