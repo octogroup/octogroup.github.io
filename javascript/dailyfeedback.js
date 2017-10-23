ec6ffd8006af8871b49ca0e9fc5506b28e7b2d80
elin kviberg <elin_kviberg@hotmail.com>
2017-10-23 13:37:37 +0200

    add new daily feedback to a card + add transition. remove old daily feedback

--- javascript/dailyfeedback.js
+++ javascript/dailyfeedback.js
@@ -1,51 +1,20 @@
 
-// Stäng rutan om användaren klickar utanför
-window.onclick = function(event) {
-	modal = document.getElementById('dfbbox');
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-function showDailyFeedBack() {
-	document.getElementById('dfbbox').style.display='flex';
-}
-
-function hideDailyFeedBack() {
-	document.getElementById('dfbbox').style.display='none';
-}
-
-function showAnswerOnSend() {
-	document.getElementById('answerOnSend').style.display='flex';
-}
-
-function hideAnswerOnSend() {
-	document.getElementById('answerOnSend').style.display='none';
-}
-
-
-//changes the current smiley image to yellow and all the other smiley images to grey onmouseover and onclick
-function checkSadSmiley () {
+//byt smileybild från grå till gul onmouseover
+function hoverSadSmiley () {
 	document.getElementById("ledsenSmiley").src = "../img/sad.png";
-	document.getElementById("neutralSmiley").src = "../img/confusedDis.png";
-	document.getElementById("happySmiley").src = "../img/happyyDis.png";z
 }
 
-function checkNeutralSmiley () {
+function hoverNeutralSmiley () {
 	document.getElementById("neutralSmiley").src = "../img/confused.png";
-	document.getElementById("ledsenSmiley").src = "../img/sadDis.png";
-	document.getElementById("happySmiley").src = "../img/happyyDis.png";
-	
 }
 
-function checkHappySmiley () {
+function hoverkHappySmiley () {
 	document.getElementById("happySmiley").src = "../img/happyy.png";
-	document.getElementById("ledsenSmiley").src = "../img/sadDis.png";
-	document.getElementById("neutralSmiley").src = "../img/confusedDis.png";
-
 }
 
 
+// onmouseout. kollar om någon av smileysirna/radiobuttons är icheckade. Är de icheckade? byt bild från grå till gul(annars ska bilden bli grå), gör höjden på id="smileyfaces" 0px och submitta formen
+//på vanligt klick så kommer en smiley/radiobutton bli icheckad och funktionen kommer att köras igen
 function resetSmileys() {
 	var sadOptionIsChecked = document.getElementById("ledsen").checked;
 	var neutralOptionIsChecked = document.getElementById("neutral").checked;
@@ -53,19 +22,27 @@ function resetSmileys() {
 
 	if(sadOptionIsChecked == true){
 		document.getElementById("ledsenSmiley").src = "../img/sad.png";
+		document.getElementById("smileyfaces").style.height = "0px"
+		document.getElementById("dailyFeedback_form").subtmit();
+
 	}else{
 		document.getElementById("ledsenSmiley").src = "../img/sadDis.png";
 	}
 
 	if(neutralOptionIsChecked == true){
 		document.getElementById("neutralSmiley").src = "../img/confused.png";
+		document.getElementById("smileyfaces").style.height = "0px"
+		document.getElementById("dailyFeedback_form").subtmit();
 	}else{
 		document.getElementById("neutralSmiley").src = "../img/confusedDis.png";
 	}
 
 	if(happyOptionIsChecked == true){
 		document.getElementById("happySmiley").src = "../img/happyy.png";
+		document.getElementById("smileyfaces").style.height = "0px"
+		document.getElementById("dailyFeedback_form").subtmit();
 	}else{
 		document.getElementById("happySmiley").src = "../img/happyyDis.png";
 	}
+
 }
