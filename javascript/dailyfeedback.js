2dc511407564a2a55a5d83528b3db2e4195ced16
Virrevirre <vike.pet@hotmail.com>
2017-10-24 14:37:12 +0200

    Make attendance button work as intended again.

--- javascript/dailyfeedback.js
+++ javascript/dailyfeedback.js
@@ -1,48 +1,67 @@
 
 //byt smileybild från grå till gul onmouseover
-function hoverSadSmiley () {
-	document.getElementById("ledsenSmiley").src = "../img/sad.png";
+function hoverSadSmiley() {
+  document.getElementById("ledsenSmiley").src = "../img/sad.png";
 }
 
-function hoverNeutralSmiley () {
-	document.getElementById("neutralSmiley").src = "../img/confused.png";
+function hoverNeutralSmiley() {
+  document.getElementById("neutralSmiley").src = "../img/confused.png";
 }
 
-function hoverHappySmiley () {
-	document.getElementById("happySmiley").src = "../img/happyy.png";
+function hoverHappySmiley() {
+  document.getElementById("happySmiley").src = "../img/happyy.png";
 }
 
 
 // onmouseout. kollar om någon av smileysirna/radiobuttons är icheckade. Är de icheckade? byt bild från grå till gul(annars ska bilden bli grå), gör höjden på id="smileyfaces" 0px och submitta formen
 //på vanligt klick så kommer en smiley/radiobutton bli icheckad och funktionen kommer att köras igen
 function checkSmileyStatus() {
-	var sadOptionIsChecked = document.getElementById("ledsen").checked;
-	var neutralOptionIsChecked = document.getElementById("neutral").checked;
-	var happyOptionIsChecked = document.getElementById("glad").checked;
+  var sadOptionIsChecked = document.getElementById("ledsen").checked;
+  var neutralOptionIsChecked = document.getElementById("neutral").checked;
+  var happyOptionIsChecked = document.getElementById("glad").checked;
 
-	if(sadOptionIsChecked){
-		document.getElementById("ledsenSmiley").src = "../img/sad.png";
-		document.getElementById("smileyfaces").style.height = "0px"
-		document.getElementById("dailyFeedback_form").subtmit();
+  if (sadOptionIsChecked) {
+    document.getElementById("ledsenSmiley").src = "../img/sad.png";
+    document.getElementById("smileyfaces").style.height = "0px"
+    document.getElementById("dailyFeedback_form").subtmit();
 
-	}else{
-		document.getElementById("ledsenSmiley").src = "../img/sadDis.png";
-	}
+  } else {
+    document.getElementById("ledsenSmiley").src = "../img/sadDis.png";
+  }
 
-	if(neutralOptionIsChecked){
-		document.getElementById("neutralSmiley").src = "../img/confused.png";
-		document.getElementById("smileyfaces").style.height = "0px"
-		document.getElementById("dailyFeedback_form").subtmit();
-	}else{
-		document.getElementById("neutralSmiley").src = "../img/confusedDis.png";
-	}
+  if (neutralOptionIsChecked) {
+    document.getElementById("neutralSmiley").src = "../img/confused.png";
+    document.getElementById("smileyfaces").style.height = "0px"
+    document.getElementById("dailyFeedback_form").subtmit();
+  } else {
+    document.getElementById("neutralSmiley").src = "../img/confusedDis.png";
+  }
 
-	if(happyOptionIsChecked){
-		document.getElementById("happySmiley").src = "../img/happyy.png";
-		document.getElementById("smileyfaces").style.height = "0px"
-		document.getElementById("dailyFeedback_form").subtmit();
-	}else{
-		document.getElementById("happySmiley").src = "../img/happyyDis.png";
-	}
+  if (happyOptionIsChecked) {
+    document.getElementById("happySmiley").src = "../img/happyy.png";
+    document.getElementById("smileyfaces").style.height = "0px"
+    document.getElementById("dailyFeedback_form").subtmit();
+  } else {
+    document.getElementById("happySmiley").src = "../img/happyyDis.png";
+  }
 
+}
+
+window.onclick = function (event) {
+  modal = document.getElementById('dfbbox');
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+function showDailyFeedBack() {
+  document.getElementById('dfbbox').style.display = 'flex';
+}
+function hideDailyFeedBack() {
+  document.getElementById('dfbbox').style.display = 'none';
+}
+function showAnswerOnSend() {
+  document.getElementById('answerOnSend').style.display = 'flex';
+}
+function hideAnswerOnSend() {
+  document.getElementById('answerOnSend').style.display = 'none';
 }
