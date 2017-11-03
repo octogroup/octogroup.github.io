ab31a5edb8811a8fa3411cc40b5722c514eb2d20
Elingustafsson <elgusen@hotmail.com>
2017-11-03 17:01:32 +0100

    add Multi Series Bar Charts to courseevaluation

--- javascript/course.js
+++ javascript/course.js
@@ -1,24 +1,119 @@
-window.onload = function() {
-
-var chart = new CanvasJS.Chart("chartContainer", {
+let chart = new CanvasJS.Chart("chartContainer1", {
 	animationEnabled: true,
-	title: {
-		text: "Desktop Search Engine Market Share - 2016"
+	title:{
+		text: "Feedback för avslutad kurs"
+	},
+	axisY: {
+		title: "Antal"
+	},
+	axisX: {
+		title: "Skala (1-7)"
+	},
+	legend: {
+		cursor:"pointer",
+		itemclick : toggleDataSeries
+	},
+	toolTip: {
+		shared: true,
 	},
 	data: [{
-		type: "pie",
-		startAngle: 240,
-		yValueFormatString: "##0.00\"%\"",
-		indexLabel: "{label} {y}",
+		type: "bar",
+		showInLegend: true,
+		name: "Hur tycker du kurser har varit?",
+		color: "gold",
 		dataPoints: [
-			{y: 79.45, label: "Hur tycker du kurser har varit?"},
-			{y: 7.31, label: "Hur har du upplevt tempot i kursen?"},
-			{y: 7.06, label: "Hur har lokalerna och lokalbokningen varit under kursens gång?"},
-			// {y: 4.91, label: "Yahoo"},
-			// {y: 1.26, label: "Others"}
+			{ y: 1, label: "1" },
+			{ y: 3, label: "2" },
+			{ y: 5, label: "3" },
+			{ y: 7, label: "4" },
+			{ y: 10, label: "5" },
+			{ y: 8, label: "6" },
+			{ y: 1, label: "7" }
+		]
+	},
+	{
+		type: "bar",
+		showInLegend: true,
+		name: "Hur har du upplevt tempot i kursen?",
+		color: "silver",
+		dataPoints: [
+			{ y: 2, label: "1" },
+			{ y: 5, label: "2" },
+			{ y: 1, label: "3" },
+			{ y: 5, label: "4" },
+			{ y: 15, label: "5" },
+			{ y: 4, label: "6" },
+			{ y: 3, label: "7" }
+		]
+	},
+	{
+		type: "bar",
+		showInLegend: true,
+		name: "Hur har lokalerna och lokalbokningen varit under kursens gång?",
+		color: "#A57164",
+		dataPoints: [
+			{ y: 3, label: "1" },
+			{ y: 2, label: "2" },
+			{ y: 8, label: "3" },
+			{ y: 7, label: "4" },
+			{ y: 10, label: "5" },
+			{ y: 3, label: "6" },
+			{ y: 1, label: "7" }
 		]
 	}]
 });
 chart.render();
 
+function toolTipFormatter(e) {
+	var str = "";
+	var total = 0 ;
+	var str3;
+	var str2 ;
+	for (var i = 0; i < e.entries.length; i++){
+		var str1 = "<span style= \"color:"+e.entries[i].dataSeries.color + "\">" + e.entries[i].dataSeries.name + "</span>: <strong>"+  e.entries[i].dataPoint.y + "</strong> <br/>" ;
+		total = e.entries[i].dataPoint.y + total;
+		str = str.concat(str1);
+	}
+	str2 = "<strong>" + e.entries[0].dataPoint.label + "</strong> <br/>";
+	str3 = "<span style = \"color:Tomato\">Total: </span><strong>" + total + "</strong><br/>";
+	return (str2.concat(str)).concat(str3);
+}
+
+function toggleDataSeries(e) {
+	if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+		e.dataSeries.visible = false;
+	}
+	else {
+		e.dataSeries.visible = true;
+	}
+	chart.render();
+}
+
+
+// Knappar
+function expand1 () {
+  let x = document.getElementById('q5')
+  if (x.style.display === 'flex') {
+    x.style.display = 'none'
+  } else {
+    x.style.display = 'flex'
+  }
+}
+
+function expand2 () {
+  let x = document.getElementById('q6')
+  if (x.style.display === 'flex') {
+    x.style.display = 'none'
+  } else {
+    x.style.display = 'flex'
+  }
+}
+
+function expand3 () {
+  let x = document.getElementById('q7')
+  if (x.style.display === 'flex') {
+    x.style.display = 'none'
+  } else {
+    x.style.display = 'flex'
+  }
 }
