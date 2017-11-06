866c91c42fe4ea1d83993479806a8b34e35faac1
elin kviberg <elin_kviberg@hotmail.com>
2017-11-06 15:06:16 +0100

    make grade required for submit

--- javascript/gradeAssignments.js
+++ javascript/gradeAssignments.js
@@ -3,14 +3,19 @@ function openAssignmentBox () {
   document.getElementById("boxContainer").style.height = "auto";
 }
 function submitGrade () {
-  alert("Betyget har nu blivit satt");
-  document.getElementById("boxContainer").style.visibility = "hidden";
-  document.getElementById("boxContainer").style.height = "0px";
 
-  if(document.getElementById("li_1").style.display != "none"){
-    document.getElementById("li_1").style.display = "none";
-  }else if(document.getElementById("li_1").style.display == "none"){
-    document.getElementById("li_2").style.display = "none";
+  if(document.getElementById("selectGrade").value == "IG" || document.getElementById("selectGrade").value == "G" ||document.getElementById("selectGrade").value == "VG"){
+    alert("Betyget har nu blivit satt");
+    document.getElementById("boxContainer").style.visibility = "hidden";
+    document.getElementById("boxContainer").style.height = "0px";
+
+    if(document.getElementById("li_1").style.display != "none"){
+      document.getElementById("li_1").style.display = "none";
+    }else if(document.getElementById("li_1").style.display == "none"){
+      document.getElementById("li_2").style.display = "none";
+    }
+  }else{
+    document.getElementById("msg").innerHTML = "Du måste ange ett betyg";
   }
 }
 
