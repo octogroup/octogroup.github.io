df00547ace9c472ddf01eaec27db07ba7fcc9d85
Virrevirre <vike.pet@hotmail.com>
2017-11-07 02:21:43 +0100

    Make content in the cards relevant. Implement js solution for opening and closing cards.

--- javascript/assignments.js
+++ javascript/assignments.js
@@ -1,8 +1,26 @@
 
-function show() {
-  document.getElementById('cardC').style.display = 'flex';
+var x = document.querySelectorAll('.hiddenBut, .hiddenCard, .cardbutton');
+
+function open1() {
+  x[0].style.display = 'flex';
+  x[1].style.display = 'none';
+  x[2].style.display = 'flex';
 }
 
-function show2() {
-  document.getElementsByClassName('hiddenCard').style.display = 'flex';
+function open2() {
+  x[3].style.display = 'flex';
+  x[4].style.display = 'none';
+  x[5].style.display = 'flex';
+}
+
+function close1() {
+  x[0].style.display = 'none';
+  x[1].style.display = 'flex';
+  x[2].style.display = 'none';
+}
+
+function close2() {
+  x[3].style.display = 'none';
+  x[4].style.display = 'flex';
+  x[5].style.display = 'none';
 }
