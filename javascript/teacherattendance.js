6b2032d5a45ceff0fca3d7edd7b8280f9984e5df
Svahnen <svahn.jonny@gmail.com>
2017-11-09 13:13:28 +0100

    Test different colors

---
+++ javascript/teacherattendance.js
@@ -0,0 +1,6 @@
+let changeColor = function (element) {
+  element.style.backgroundColor = 'green'
+  if (element.style.backgroundColor === 'green') {
+    element.style.backgroundColor = 'red'
+  }
+}
