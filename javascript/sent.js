52684924ee5a5e03037bfc6d427a11324183f976
mirandamutka <michirisu@outlook.com>
2017-11-21 16:26:57 +0100

    remove unused code in sent.js and add link to sent.js in assignments.html

--- javascript/sent.js
+++ javascript/sent.js
@@ -7,18 +7,6 @@ window.onclick = function (event) {
   }
 }
 
-function toggleAlertbox (page) {
-	let alertModul = document.getElementById('alertbox-modulus')
-	let alertStatus = alertModul.style.display
-	if (alertStatus === 'none' || alertStatus == '') {
-		alertModul.style.display = 'flex'
-		setInterval(() => window.location.href = page, 2000)
-	} else {
-		alertModul.style.display = 'none'
-	}
-	return false
-}
-
 function toggleAlertbox (page, msg) {
 	document.getElementsByTagName('main')[0].innerHTML += '<div id="alertbox-modulus"><div class="alertbox-content zoom"><img src="../img/check.svg" alt="check" class="card-img alert-img"><h2 class="alertbox-header alertMsg"></h2><p class="alertbox-subheader"></p></div></div>'
 	let alertModul = document.getElementById('alertbox-modulus')
